5b883fb91d198211e09a7f2f1f94b1a3e61dc2e8
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-09-01 10:42:41 +0200

    rajout du job 06

---
+++ jour01/job06/job06.cs
@@ -0,0 +1,28 @@
+﻿class job06
+{
+    static void identify( int niveau)
+    {
+        if (niveau ==0 )
+        {
+        Console.WriteLine("Sans danger");
+        }
+
+        else if (niveau >= 1 && niveau <= 4)
+        {
+            Console.WriteLine("Peu dangeureux");
+        }
+
+        else
+        {
+            Console.WriteLine("Dangereux");
+        }
+
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Donner le niveau de dangerosité");
+        int niveau = int.Parse(Console.ReadLine());
+        identify(niveau);
+    }
+}
