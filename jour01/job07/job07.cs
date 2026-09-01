cfad585e432fcb4e333e3cd4259980e55289be55
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-09-01 10:56:00 +0200

    rajout du job 7

---
+++ jour01/job07/job07.cs
@@ -0,0 +1,16 @@
+﻿class job07
+{
+    static void ListTarget(string[] targets)
+    {
+        for (int i = 0; i < targets.Length; i++)
+        {
+            Console.WriteLine(targets[i]);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        string[] targets ={ "Cible 1", "Cible 2", "Cible 3", "Cible 4", "Cible 5" };
+        ListTarget(targets);
+    }
+}
