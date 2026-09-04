69c0c0fa80ea5472f307d7527bfe6da43c46102b
Mohamed <mohamed.mahamoud0112@gmail.com>
2026-09-04 10:42:27 +0200

    rajout du job 05

---
+++ jour04/job05/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Ritual;
+
+class Program
+{
+    static void Main()
+    {
+        TeleportRitual ritual = new TeleportRitual();
+        ritual.ProgressChanged += Ritual_ProgressChanged;
+        ritual.ProgressChanged += FluxStability_ProgressChanged;
+
+        ritual.AlignRunes();
+        ritual.PurifyMiasmaStream();
+        ritual.StabilizeRift();
+    }
+
+    private static void Ritual_ProgressChanged(object? sender, RitualProgressEventArgs e)
+    {
+        Console.WriteLine($"Étape : {e.StepName}, Progression : {e.ProgressPercentage}%, Stabilité du flux : {e.FluxStability}%");
+    }
+
+    private static int? previousFluxStability;
+
+    private static void FluxStability_ProgressChanged(object? sender, RitualProgressEventArgs e)
+    {
+        if (previousFluxStability.HasValue && e.FluxStability < previousFluxStability.Value - 20)
+        {
+            Console.WriteLine($"ALERTE : chute importante de la stabilité du flux ({e.FluxStability}%).");
+        }
+        else
+        {
+            Console.WriteLine($"Surveillance du flux : stabilité à {e.FluxStability}%.");
+        }
+
+        previousFluxStability = e.FluxStability;
+    }
+}
---
+++ jour04/job05/TeleportRitual.cs
@@ -0,0 +1,47 @@
+﻿namespace Ritual;
+
+public class RitualProgressEventArgs : EventArgs
+{
+    public string StepName { get; }
+    public int ProgressPercentage { get; }
+    public int FluxStability { get; }
+
+    public RitualProgressEventArgs(string stepName, int progressPercentage, int fluxStability)
+    {
+        StepName = stepName;
+        ProgressPercentage = progressPercentage;
+        FluxStability = fluxStability;
+    }
+}
+
+public class TeleportRitual
+{
+    public event EventHandler<RitualProgressEventArgs>? ProgressChanged;
+
+    public int ProgressPercentage { get; private set; }
+    public int FluxStability { get; private set; }
+
+    public void AlignRunes()
+    {
+        UpdateProgress("AlignRunes", 33, 70);
+    }
+
+    public void PurifyMiasmaStream()
+    {
+        UpdateProgress("PurifyMiasmaStream", 66, 85);
+    }
+
+    public void StabilizeRift()
+    {
+        UpdateProgress("StabilizeRift", 100, 100);
+    }
+
+    private void UpdateProgress(string stepName, int progressPercentage, int fluxStability)
+    {
+        ProgressPercentage = progressPercentage;
+        FluxStability = fluxStability;
+        ProgressChanged?.Invoke(
+            this,
+            new RitualProgressEventArgs(stepName, ProgressPercentage, FluxStability));
+    }
+}
